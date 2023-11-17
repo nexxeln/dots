c4df454b78047beb1dc994c7238a837fd592acd8
Shoubhit Dash <shoubhit2005@gmail.com>
2023-11-17 19:05:41 +0530

    add some remaps

--- .config/nvim/lua/nexxel/core/keymap.lua
+++ .config/nvim/lua/nexxel/core/keymap.lua
@@ -1,1 +1,15 @@
+local map = vim.keymap.set
+
 vim.g.mapleader = " "
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "J", "mzJ`z")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "<leader>n", "<cmd>nohlsearch<CR>")
+map("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
