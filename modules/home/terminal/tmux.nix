1f6099387ce8b53a4d4b5c3412f207d6c3e85733
Shoubhit Dash <shoubhit2005@gmail.com>
2026-01-21 15:03:22 +0530

    add more windows

--- modules/home/terminal/tmux.nix
+++ modules/home/terminal/tmux.nix
@@ -81,6 +81,10 @@ in
       bind -n M-3 select-window -t 3
       bind -n M-4 select-window -t 4
       bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t 9
       bind -n M-n next-window
       bind -n M-p previous-window
 
